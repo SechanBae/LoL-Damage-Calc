2a675968184e58b602859db43be9b9317d9234f3
SechanBae <sechan.bae@gmail.com>
2021-01-31 18:59:35 -0500

    start files

---
+++ champ-stats.js
@@ -0,0 +1,502 @@
+var championStats={
+    "Aatrox": {
+        "health":580,
+        "healthGrowth":90,
+        "armor":38,
+        "armorGrowth":3.25,
+        "mr":32,
+        "mrGrowth":1.25,
+        "ms":345,
+        "ad":60,
+        "adGrowth":5,
+        "as":0.651,
+        "asGrowth":0.025,
+        "crit":1.75,
+
+        
+    },
+    "Ahri":{
+        "health":526,
+        "healthGrowth":92,
+        "mana":418,
+        "manaGrowth":25,
+        "armor":21,
+        "armorGrowth":3.5,
+        "mr":30,
+        "mrGrowth":0.5,
+        "ms":330,
+        "ad":53,
+        "adGrowth":3,
+        "as":0.668,
+        "asGrowth":0.02,
+        "crit":1.75,
+    },
+    "Akali":{
+        "health":575,
+        "healthGrowth":95,
+        "armor":23,
+        "armorGrowth":3.5,
+        "mr":37,
+        "mrGrowth":1.25,
+        "ms":345,
+        "ad":62,
+        "adGrowth":3.3,
+        "as":0.625,
+        "asGrowth":0.032,
+        "crit":1.75,
+
+    },
+    "Alistar":{
+
+    },
+    "Amumu":{
+
+    },
+    "Anivia":{
+
+    },
+    "Annie":{
+
+    },
+    "Aphelios":{
+
+    },
+    "Ashe":{
+
+    },
+    "Aurelion Sol":{
+
+    },
+    "Azir":{
+
+    },
+    "Bard":{
+
+    },
+    "Blitzcrank":{
+
+    },
+    "Brand":{
+
+    },
+    "Braum":{
+
+    },
+    "Caitlyn":{
+
+    },
+    "Camille":{
+
+    },
+    "Cassiopeia":{
+
+    },
+    "Cho'Gath":{
+
+    },
+    "Corki":{
+
+    },
+    "Darius":{
+
+    },
+    "Diana":{
+
+    },
+    "Dr.Mundo":{
+
+    },
+    "Draven":{
+
+    },
+    "Ekko":{
+
+    },
+    "Elise":{
+
+    },
+    "Evelynn":{
+
+    },
+    "Ezreal":{
+
+    },
+    "Fiddlesticks":{
+
+    },
+    "Fiora":{
+
+    },
+    "Fizz":{
+
+    },
+    "Galio":{
+
+    },
+    "Gangplank":{
+
+    },
+    "Garen":{
+
+    },
+    "Gnar":{
+
+    },
+    "Gragas":{
+
+    },
+    "Graves":{
+
+    },
+    "Hecarim":{
+
+    },
+    "Heimerdinger":{
+
+    },
+    "Illaoi":{
+
+    },
+    "Irelia":{
+
+    },
+    "Ivern":{
+
+    },
+    "Janna":{
+
+    },
+    "Jarvan IV":{
+
+    },
+    "Jax":{
+
+    },
+    "Jayce":{
+
+    },
+    "Jhin":{
+
+    },
+    "Jinx":{
+
+    },
+    "Kai'Sa":{
+
+    },
+    "Kalista":{
+
+    },
+    "Karma":{
+
+    },
+    "Karthus":{
+
+    },
+    "Kassadin":{
+
+    },
+    "Katarina":{
+
+    },
+    "Kayle":{
+
+    },
+    "Kayne":{
+
+    },
+    "Kennen":{
+
+    },
+    "Kha'Zix":{
+
+    },
+    "Kindred":{
+
+    },
+    "Kled":{
+
+    },
+    "Kog'Maw":{
+
+    },
+    "Leblanc":{
+
+    },
+    "Lee Sin":{
+
+    },
+    "Leona":{
+
+    },
+    "Lilia":{
+
+    },
+    "Lissandra":{
+
+    },
+    "Lucian":{
+
+    },
+    "Lulu":{
+
+    },
+    "Lux":{
+
+    },
+    "Malphite":{
+
+    },
+    "Malzahar":{
+
+    },
+    "Maokai":{
+
+    },
+    "Master Yi":{
+
+    },
+    "Miss Fortune":{
+
+    },
+    "Mordekaiser":{
+
+    },
+    "Morgana":{
+
+    },
+    "Nami":{
+
+    },
+    "Nasus":{
+
+    },
+    "Nautilus":{
+
+    },
+    "Neeko":{
+
+    },
+    "Nidalee":{
+
+    },
+    "Nocturne":{
+
+    },
+    "Nunu":{
+
+    },
+    "Olaf":{
+
+    },
+    "Oriana":{
+
+    },
+    "Ornn":{
+
+    },
+    "Pantheon":{
+
+    },
+    "Poppy":{
+
+    },
+    "Pyke":{
+
+    },
+    "Qiyana":{
+
+    },
+    "Quinn":{
+
+    },
+    "Rakan":{
+
+    },
+    "Rammus":{
+
+    },
+    "Rek'Sai":{
+
+    },
+    "Rell":{
+
+    },
+    "Renekton":{
+
+    },
+    "Rengar":{
+
+    },
+    "Riven":{
+
+    },
+    "Rumble":{
+
+    },
+    "Ryze":{
+
+    },
+    "Samira":{
+
+    },
+    "Sejuani":{
+
+    },
+    "Senna":{
+
+    },
+    "Seraphine":{
+
+    },
+    "Sett":{
+
+    },
+    "Shaco":{
+
+    },
+    "Shen":{
+
+    },
+    "Shyvana":{
+
+    },
+    "Singed":{
+
+    },
+    "Sion":{
+
+    },
+    "Sivir":{
+
+    },
+    "Skarner":{
+
+    },
+    "Sona":{
+
+    },
+    "Soraka":{
+
+    },
+    "Swain":{
+
+    },
+    "Sylas":{
+
+    },
+    "Syndra":{
+
+    },
+    "Tahm Kench":{
+
+    },
+    "Taliyah":{
+
+    },
+    "Talon":{
+
+    },
+    "Taric":{
+
+    },
+    "Teemo":{
+
+    },
+    "Thresh":{
+
+    },
+    "Tristana":{
+
+    },
+    "Trundle":{
+
+    },
+    "Tryndamere":{
+
+    },
+    "Twisted Fate":{
+
+    },
+    "Twitch":{
+
+    },
+    "Udyr":{
+
+    },
+    "Urgot":{
+
+    },
+    "Varus":{
+
+    },
+    "Vayne":{
+
+    },
+    "Veigar":{
+
+    },
+    "Vel'Koz":{
+
+    },
+    "Vi":{
+
+    },
+    "Viego":{
+
+    },
+    "Viktor":{
+
+    },
+    "Vladimir":{
+
+    },
+    "Volibear":{
+
+    },
+    "Warwick":{
+
+    },
+    "Wukong":{
+
+    },
+    "Xayah":{
+
+    },
+    "Xerath":{
+
+    },
+    "Xin Zhao":{
+
+    },
+    "Yasuo":{
+
+    },
+    "Yone":{
+
+    },
+    "Yorick":{
+
+    },
+    "Yuumi":{
+
+    },
+    "Zac":{
+
+    },
+    "Zed":{
+
+    },
+    "Ziggs":{
+
+    },
+    "Zilean":{
+
+    },
+    "Zoe":{
+
+    },
+    "Zyra":{
+
+    }
+}
